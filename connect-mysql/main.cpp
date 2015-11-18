cd66a1fbf45085fd6f3138317da7cfe6be312c37
细佬 <allen@allendemacbook-pro.local>
2015-11-18 22:27:57 +0800

    Initial Commit

---
+++ connect-mysql/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  connect-mysql
+//
+//  Created by Allen on 15/11/18.
+//  Copyright © 2015年 Allen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
